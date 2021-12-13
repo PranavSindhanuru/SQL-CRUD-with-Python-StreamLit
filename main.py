4b61e68631715a04e91c26bc8f26643e47e247ea
PranavSindhanuru <pranavsindhanuru@gmail.com>
2021-12-13 13:05:14 +0530

    update

--- main.py
+++ main.py
@@ -22,6 +22,23 @@ mycursor = db.cursor()
 # mycursor.execute("SHOW TABLES")
 # for x in mycursor:
 #   print(x)
+mycursor.execute("SELECT * FROM guest")
+guest = pd.DataFrame(mycursor.fetchall(), columns=['Guest ID', 'First Name', 'Last Name', 'Address', 'Phone number', 'Email'])
+
+mycursor.execute("SELECT * FROM reservation")
+reservation = pd.DataFrame(mycursor.fetchall(), columns=['Reservation ID', 'Guest ID', 'Room Type ID', 'Check IN', 'Check OUT', 'Reservation Status'])
+
+mycursor.execute("SELECT * FROM booking")
+booking = pd.DataFrame(mycursor.fetchall(), columns=['Booking Id', 'Room Type ID','Guest_ID', 'Check IN', 'Check OUT'])
+
+mycursor.execute("SELECT * FROM room_type")
+room_type = pd.DataFrame(mycursor.fetchall(), columns=['Room Type ID', 'Room Type'])
+
+mycursor.execute("SELECT * FROM room")
+room = pd.DataFrame(mycursor.fetchall(), columns=['Room ID', 'Room Number', 'Room Type ID', 'Room Price', 'Room Status'])
+
+mycursor.execute("SELECT * FROM payment")
+payment = pd.DataFrame(mycursor.fetchall(), columns=['Payment ID', 'Guest ID', 'Reservation ID', 'Add ON', 'Room Price', 'Number of Nights', 'Total', 'Payment Status'])
 
 header = st.container()
 
@@ -40,66 +57,71 @@ with st.form(key='datainput'):
     room_type_id = st.number_input('Room Type ID', step=1, min_value=0)
     check_in = st.text_input('Check In')
     check_out = st.text_input('Check Out')
-    reservation_status = st.number_input('Reservation Status', step=1, min_value=0)
+    reservation_status = st.number_input('Reservation Status', step=1, min_value=0, max_value=1)
     booking_id = st.number_input('Booking ID', step=1, min_value=0)
     room_id = st.number_input('Room ID', step=1, min_value=0)
-    room_type = st.text_input('Room Type')
+    room_type_ = st.text_input('Room Type')
     room_number = st.number_input('Room Number', step=1, min_value=0)
     room_price = st.number_input('Room Price', step=1, min_value=0)
-    room_status = st.number_input('Room Status', step=1, min_value=0)
+    room_status = st.number_input('Room Status', step=1, min_value=0, max_value=1)
     payment_id = st.number_input('Payment ID', step=1, min_value=0)
     add_on = st.text_input('Add on extra features')
     number_of_nights = st.number_input('Number of Nights', step=1, min_value=0)
     total = st.number_input('Total', step=1, min_value=0)
-    payment_status = st.number_input('Payment Status', step=1, min_value=0)
+    payment_status = st.number_input('Payment Status', step=1, min_value=0, max_value=1)
     submit_button = st.form_submit_button()
 
 if submit_button:
-    if guest_id != 0:
-        mycursor.execute("INSERT INTO guest VALUES (%s, %s, %s, %s, %s, %s)", (guest_id, first_name, last_name, address, ph_no, email))
-    if reservation_id != 0:
-        mycursor.execute("INSERT INTO reservation VALUES (%s, %s, %s, %s, %s, %s);", (reservation_id, guest_id, room_type_id, check_in, check_out, reservation_status))
-    if booking_id != 0:
-        mycursor.execute("INSERT INTO booking VALUES (%s, %s, %s, %s, %s);", (booking_id, room_type_id, guest_id, check_in, check_out))
-    if room_type_id != 0:
-        mycursor.execute("INSERT INTO room_type VALUES (%s, %s);", (room_type_id, room_type))
-    if room_id != 0:
-        mycursor.execute("INSERT INTO room VALUES (%s, %s, %s, %s, %s);", (room_id, room_number, room_type_id, room_price, room_status))
-    if payment_id != 0:
-        mycursor.execute("INSERT INTO payment VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (payment_id, guest_id, reservation_id, add_on, room_price, number_of_nights, total, payment_status))
-    db.commit()
+    with st.spinner('Running ....'):
+        if guest_id != 0 and guest_id not in guest['Guest ID']:
+            mycursor.execute("INSERT INTO guest VALUES (%s, %s, %s, %s, %s, %s)", (guest_id, first_name, last_name, address, ph_no, email))
+        if reservation_id != 0:
+            mycursor.execute("INSERT INTO reservation VALUES (%s, %s, %s, %s, %s, %s);", (reservation_id, guest_id, room_type_id, check_in, check_out, reservation_status))
+        if booking_id != 0:
+            mycursor.execute("INSERT INTO booking VALUES (%s, %s, %s, %s, %s);", (booking_id, room_type_id, guest_id, check_in, check_out))
+        if room_type_id != 0:
+            mycursor.execute("INSERT INTO room_type VALUES (%s, %s);", (room_type_id, room_type_))
+        if room_id != 0:
+            mycursor.execute("INSERT INTO room VALUES (%s, %s, %s, %s, %s);", (room_id, room_number, room_type_id, room_price, room_status))
+        if payment_id != 0:
+            mycursor.execute("INSERT INTO payment VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (payment_id, guest_id, reservation_id, add_on, room_price, number_of_nights, total, payment_status))
+        db.commit()
+    st.success('Done!')
+
+st.button('Refresh Page')
 
 st.write('GUEST')
-mycursor.execute("SELECT * FROM guest")
-guest = pd.DataFrame(mycursor.fetchall(), columns=['Gust ID', 'First Name', 'Last Name', 'Address', 'Phone number', 'Email'])
 guest
 
 st.write('RESERVATION')
-mycursor.execute("SELECT * FROM reservation")
-reservation = pd.DataFrame(mycursor.fetchall(), columns=['Reservation ID', 'Guest ID', 'Room Type ID', 'Check IN', 'Check OUT', 'Reservation Status'])
 reservation
 
 st.write('BOOKING')
-mycursor.execute("SELECT * FROM booking")
-booking = pd.DataFrame(mycursor.fetchall(), columns=['Booking Id', 'Room Type ID','Guest_ID', 'Check IN', 'Check OUT'])
 booking
 
 st.write('ROOM TYPE')
-mycursor.execute("SELECT * FROM room_type")
-room_type = pd.DataFrame(mycursor.fetchall(), columns=['Room Type ID', 'Room Type'])
 room_type
 
 st.write('ROOM')
-mycursor.execute("SELECT * FROM room")
-room = pd.DataFrame(mycursor.fetchall(), columns=['Room ID', 'Room Number', 'Room Type ID', 'Room Price', 'Room Status'])
 room
 
 st.write('PAYMENTS')
-mycursor.execute("SELECT * FROM payment")
-payment = pd.DataFrame(mycursor.fetchall(), columns=['Payment ID', 'Guest ID', 'Reservation ID', 'Add ON', 'Room Price', 'Number of Nights', 'Total', 'Payment Status'])
 payment
 
+with st.form('deletedata'):
+    st.subheader('Delete Row')
+    table = st.selectbox('Table', ['Guest', 'Reservation', 'Booking', 'Room Type', 'Room', 'Payment'])
+    str_x = table.lower().replace(' ', '_')
+    str_x_id = str_x + '_id'
+    id = st.number_input('ID', step=1, min_value=0)
+    delete_button = st.form_submit_button('DELETE')
 
+if delete_button:
+    try:
+        mycursor.execute(f"DELETE FROM {str_x} WHERE {str_x_id} = '{id}'")
+        db.commit()
+    except:
+        print('Item doesn\'t exist')
 
 with st.form(key='cleardata'):
     st.subheader('!! Clear All Data !!')
